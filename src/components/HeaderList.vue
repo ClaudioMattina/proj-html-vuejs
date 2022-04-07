ef793ede30eef46995ecc483e0a870f68d4516fe
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-04-07 17:49:57 +0200

    implementazione lista dell'header

--- src/components/HeaderList.vue
+++ src/components/HeaderList.vue
@@ -1,15 +1,65 @@
 <template>
   <div>
-      
+      <ul class="d-flex justify-content-around">
+          <li v-for="(element,index) in navBarList" :key="index">
+              <a :class="{active : element.current}" href="#"> {{ element.text }} </a>
+          </li>
+      </ul>
   </div>
 </template>
 
 <script>
 export default {
+    name: "HeaderListIndex",
 
+    data: function(){
+        return{
+            navBarList:[
+                {
+                    text: "Home",
+                    current: true,
+                },
+                {
+                    text: "Recipes",
+                    current: false,
+                },
+                {
+                    text: "Places",
+                    current: false,
+                },
+                {
+                    text: "Blog",
+                    current: false,
+                },
+                {
+                    text: "About",
+                    current: false,
+                },
+                {
+                    text: "Contact",
+                    current: false,
+                },
+            ]
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
+    li{
+        list-style: none;
+        margin: 10px;
+    }
+    a{
+        text-decoration: none;
+        color: black;
+
+            &:active,
+            &.active,
+            &:hover{
+                color: #fc7525;
+                border-bottom: 2px solid #fc7525;
+            }
+    }
 </style>
