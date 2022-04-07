056b483afe5180d553924ad9f0d1ff17b0ac84a1
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-04-07 16:52:35 +0200

    top header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
+  <div class="my-container d-flex">
+
+    <div class="top-container d-flex justify-content-between">
+
+        <div id="download" class="d-flex ">
+          <p>Download App</p>
+          <div id="ios-container">
+              <a class="d-flex" href="#">iOS</a>
+          </div>
+          <p>Advertise whit us</p>
+        </div>
+
+        <div class="d-flex social">
+            <p>F</p>
+            <p>INS</p>
+            <p>TWI</p>
+            <p>YT</p>
+        </div>
+
+    </div>
       
   </div>
 </template>
@@ -10,6 +29,43 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
+
+    .my-container{
+        width: 70%;
+        margin: 0 auto;
+        height: 100px;
+        margin-top: 20px;
+
+        .top-container{
+            width: 100%;
+
+            .social{
+                p{
+                    margin: 0px 5px;
+                }
+            }
+
+            #download{
+             height: 20px;
+
+            #ios-container{
+                margin: 0px 15px 0px 10px;
+                border: 1px solid #fc7525;
+                background-color: #fc7525;
+                border-radius: 5px;
+                
+                a{
+                    text-decoration: none;
+                    color: white;
+                    padding: 0px 10px 2px 10px;
+
+                }
+            }
+            }
+        }
+         
+         
+    }
 </style>
