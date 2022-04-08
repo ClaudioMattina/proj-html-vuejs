2359f19337765402795d47eef780603ea5e03d98
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-04-08 13:05:50 +0200

    schede del main collection content

--- src/components/MainCollectionContent.vue
+++ src/components/MainCollectionContent.vue
@@ -1,15 +1,96 @@
 <template>
-  <div>
+  <div
+  >
+      <div id="title" class="row my-5">
+        <div class="col-12 d-flex flex-column">
+            <h3>CULINARY COLLECTION</h3>
+            <p>Aliquam eat volupat. Ut quis ligula a magna blamdit firibus. Suspendisse maximus lacus non nunc lacinia lobortis.</p>
+        </div>
+      </div>
+
+      <div class="row">
+
+        <div v-for="(element, index) in imagesList" :key="index" class="col-3">
+          <div class="img-container">
+            <img :src="element.url" alt="">
+          </div>
+          <div class="description d-flex justify-content-center align-items-center">
+            <h5> {{ element.text }} </h5>
+          </div>
+        </div>
+
+      </div>
 
   </div>
 </template>
 
 <script>
 export default {
-    name:"MainCollectionIndex"
+    name:"MainCollectionContent",
+
+    data:function(){
+      return{
+        imagesList: [
+          {
+            url:"../assets/materiale/drinks-recipes.png",
+            text:"DRINKS"
+          },
+          {
+            url:"../assets/materiale/soups-recipes.png",
+            text:"SOUPS"
+          },
+          {
+            url:"../assets/materiale/baking-recipes.png",
+            text:"BAKERY"
+          },
+          {
+            url:"../assets/materiale/dinner-recipes.png",
+            text:"DINNER"
+          },
+          {
+            url:"../assets/materiale/healthy-recipes.png",
+            text:"HEALTHY"
+          },
+          {
+            url:"../assets/materiale/staff-picks.png",
+            text:"STAFF PICKS"
+          },
+          {
+            url:"../assets/materiale/premium-recipes.png",
+            text:"APPETISERS"
+          },
+          {
+            url:"../assets/materiale/quick-easy-recipes.png",
+            text:"QUICK & EASY"
+          },
+        ]
+      }
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+    #title{
+      width: 70%;
+      margin: 0 auto;
+      text-align: center;
+    }
+
+    .img-container{
+      height: 150px;
+      width: 100%;
+
+        img{
+          width: 100%;
+          height: 100%;
+        }
+    }
+
+    .description{
+
+      h5{
+        font-size: 1rem;
+      }
+    }
 
 </style>
